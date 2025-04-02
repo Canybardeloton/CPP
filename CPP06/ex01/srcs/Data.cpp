452e800b3394038c873287ed30c45a75bb092a9b
Armel Gilibert <agiliber@paul-f4Br4s6.clusters.42paris.fr>
2025-04-02 10:38:46 +0200

    vf

--- CPP06/ex01/srcs/Data.cpp
+++ CPP06/ex01/srcs/Data.cpp
@@ -6,7 +6,7 @@
 /*   By: agiliber <agiliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:35:13 by agiliber          #+#    #+#             */
-/*   Updated: 2025/04/01 16:39:18 by agiliber         ###   ########.fr       */
+/*   Updated: 2025/04/02 10:06:02 by agiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,34 @@ Data::Data() : _name("Jack")
 	std::cout << "Data default constructor called." << std::endl;
 }
 
+Data::~Data()
+{
+	std::cout << "Data default destructor called." << std::endl;
+}
+
 Data::Data(Data const &src)
 {
 	std::cout << "Data copy constructor called." << std::endl;
 	*this = src;
 }
 
-Data::~Data()
-{
-	std::cout << "Data default destructor called." << std::endl;
-}
-
 Data &	Data::operator=(Data const &rSym)
 {
 	std::cout << "Data assignment called." << std::endl;
-	if (this != &rSym) {
+	if (this != &rSym)
+	{
 		this->_name = rSym._name;
 	}
-	return *this;
+	return (*this);
 }
 
 std::ostream &	operator<<(std::ostream & o, Data const &rSym)
 {
 	o << rSym.getName();
-	return o;
+	return (o);
 }
 
 std::string	Data::getName() const
 {
-	return this->_name;
+	return (this->_name);
 }

59a6e76d3a4dde4355b0793adce7fa527745e851
Armel Gilibert <agiliber@paul-f4Br4s8.clusters.42paris.fr>
2025-04-03 12:07:22 +0200

    Class template

--- CPP07/ex02/include/Array.hpp
+++ CPP07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: agiliber <agiliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 11:17:31 by agiliber          #+#    #+#             */
-/*   Updated: 2025/04/02 17:29:11 by agiliber         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:06:21 by agiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,62 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <exception>
 
-template <class T>
+template <typename T>
 class Array
 {
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array& const copy);
-		Array&	operator=(Array& const copy)
-		~Array();
+		Array() : _array(nullptr), _size(0)
+		{
+			std::cout << "Array created" << std::endl;
+		}
+
+		Array(unsigned int n) : _size(n)
+		{
+			_array = new T[n];
+			std::cout << "Array created of size " << n << std::endl;
+		}
+
+		Array(const Array& copy) : _size(copy._size)
+		{
+			memcpy(this->_array, copy._array, _size * sizeof(T));
+			std::cout << "Array created by copy" << std::endl;
+		}
+
+		Array&	operator=(const Array& copy)
+		{
+			if (this != &copy)
+			{
+				delete[] _array;
+				_size = copy._size;
+				_array = new T[_size];
+				memcpy(this->_array, copy._array, _size * sizeof(T));
+				std::cout << "Array created by assignement" << std::endl;
+			}
+			return (*this);
+		}
+
+		~Array()
+		{
+			delete [] _array;
+			std::cout << "Array destroyed" << std::endl;
+		}
+
+		unsigned long	size(void)
+		{
+			return (_size);
+		}
+
+		T&	operator[](unsigned long index)
+		{
+			if (index >= _size)
+				throw (std::exception("Index out of bounds"));
+			return (_array[index]);
+		}
 
 	private:
-
+		unsigned long	_size;
+		T	_array;
 };
--- CPP07/ex02/srcs/Array.cpp
+++ CPP07/ex02/srcs/Array.cpp
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Array.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agiliber <agiliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/02 11:17:11 by agiliber          #+#    #+#             */
-/*   Updated: 2025/04/03 11:13:07 by agiliber         ###   ########.fr       */
+/*   Created: 2025/04/03 10:00:31 by agiliber          #+#    #+#             */
+/*   Updated: 2025/04/03 11:25:25 by agiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/Iter.hpp"
-
-int main(void)
-{
-	std::string array[] = {"hello"};
-	int tab[] = {1, 2, 3, 4};
-	unsigned long length;
-
-	length = 1;
-	::iter(array, length, function<std::string>);
-	std::cout << std::endl;
-
-	length = 4;
-	::iter(tab, length, function<int>);
-
-	return 0;
-}
+#include "../include/Array.hpp"
